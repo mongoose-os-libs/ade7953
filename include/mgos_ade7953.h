15377279f1f0303c3111ca45feb81802d6e39846
Pim van Pelt <pim@ipng.nl>
2020-03-27 13:36:38 +0100

    Add a bit of documentation to the public header

--- include/mgos_ade7953.h
+++ include/mgos_ade7953.h
@@ -29,14 +29,32 @@ extern "C" {
 
 struct mgos_ade7953;
 
+// Create an instance of the driver at the given I2C bus and address.
+// Returns a pointer to the object upon success, NULL otherwise.
 struct mgos_ade7953 *mgos_ade7953_create(struct mgos_i2c *i2c, uint8_t i2caddr);
 
+// Write the detected voltage in Volts RMS in the *volts pointer.
+// Returns true on success, false otherwise.
 bool mgos_ade7953_get_voltage(struct mgos_ade7953 *dev, float *volts);
-bool mgos_ade7953_set_scale_voltage(struct mgos_ade7953 *dev, float scale);
+
+// Set scaling factor for the current shunt on the given channel (0 or 1).
+// Returns true on success, false otherwise.
 bool mgos_ade7953_set_scale_current(struct mgos_ade7953 *dev, int channel, float scale);
+
+// Set scaling factor for the volotage divider over the VP/VN pins of the chip.
+// Returns true on success, false otherwise.
+bool mgos_ade7953_set_scale_voltage(struct mgos_ade7953 *dev, float scale);
+
+// Write the detected line frequency in Hertz to the *hertz pointer.
+// Returns true on success, false otherwise.
 bool mgos_ade7953_get_frequency(struct mgos_ade7953 *dev, float *hertz);
+
+// Write the measured RMS current for the given channel (0 or 1), in Amperes
+// to the *amperes pointer.
+// Returns true on success, false otherwise.
 bool mgos_ade7953_get_current(struct mgos_ade7953 *dev, int channel, float *amperes);
 
+// CLean up the driver and return memory used for it.
 bool mgos_ade7953_destroy(struct mgos_ade7953 **dev);
 
 #ifdef __cplusplus
